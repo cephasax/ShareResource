753e589e409ea9cfdc963334a67e6079ff38cd17
cephasax <cephasax@hotmail.com>
2015-09-13 23:48:33 -0300

    update

--- src/BancoTeste.java
+++ src/BancoTeste.java
@@ -6,11 +6,11 @@ public class BancoTeste {
 		
 		int valorDoGerente = 10;
 		
-		System.out.println("Banco e Cofre criados. Valor no cofre = " + c.getValorGuardado() + "\n" );
-		for(int i = 0; i < 3; i++){
+		System.out.println("Banco e Cofre criados. Valor no inicial no cofre = " + c.getValorGuardado() + "\n" );
+		for(int i = 1; i < 4; i++){
 			GerenteRunnable gerente= new GerenteRunnable(c, valorDoGerente, i);
 			
-			System.out.println("Bom dia, Sou o gerente numero: " + i + " e tenho permissão para movimentar " + valorDoGerente + " por vez. \n");
+			System.out.println("Bom dia, Sou o gerente numero: " + i + " e tenho permissão para movimentar " + valorDoGerente + "R$ por vez. \n");
 			valorDoGerente *= 10;
 			Thread thread = new Thread(gerente);
 			thread.start();
